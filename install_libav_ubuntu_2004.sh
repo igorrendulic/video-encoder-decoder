e63ebf073ffd3472469c0f44284aaae192c0b519
igorrendulic <igor.amplio@gmail.com>
2022-07-07 14:18:42 -0600

    Added hiredis to compile linker

--- install_libav_ubuntu_2004.sh
+++ install_libav_ubuntu_2004.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+# sudo apt update
+
 sudo apt-get -y install libavcodec-dev \
     libavformat-dev libavdevice-dev \
     libswscale-dev libswresample-dev \
     libavfilter-dev libavutil-dev \
     libpostproc-dev
+
+# install hiredis
+
+
+sudo apt install redis
+sudo systemctl status redis
